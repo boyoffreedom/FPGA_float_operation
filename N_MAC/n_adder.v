1cbcbc3eb54e851b3e5ac281419413aff07a12e3
Alan <403466681@qq.com>
2018-05-21 19:45:09 +0800

    Update n_adder.v

--- N_MAC/n_adder.v
+++ N_MAC/n_adder.v
@@ -36,7 +36,7 @@ always @(negedge clk or negedge rst_n)begin
 			if(acc_start)begin				//控制器发出累加触发（乘法器已经完成运算）
 				acc_state <= 1;
 				acc_finish <= 0;
-				acc_loop <= `CELL_N>>1;		//获取累加循环次数
+				acc_loop <= 0;					//累加循环次数
 				acc_mux <= 0;					//数据选择为加法器输出数据
 			end
 			else begin
@@ -45,22 +45,20 @@ always @(negedge clk or negedge rst_n)begin
 			end
 		end
 		1:begin
-			if(acc_loop == 0)begin			//组内加法器循环完成，目前只剩三个有效数据保留在三级流水线上
+			if(acc_loop > 3*(`CELL_N>>1))begin			//组内加法器循环完成，目前只剩三个有效数据保留在三级流水线上
 				acc_buf[0] <= 0;				//使用3个缓存变量去保存这三个数据
 				acc_buf[1] <= 0;
 				acc_buf[2] <= 0;
 				acc_state <= 5;
 			end
 			else begin							//所有数据向第一个加法器内汇聚
-				acc_loop <= acc_loop >> 1;
-				acc_state <= 2;
+				acc_loop <= acc_loop+1'b1;
+				acc_state <= 1;
 			end
 		end
-		2:acc_state <= 3;
-		3:acc_state <= 1;
-		
 		5:begin									//装载三级流水线的三个数据
 			acc_state <= 6;
+			acc_mux <= 1;
 			acc_buf[0] <= adder_out[`D_LEN-1:0];
 		end
 		6:begin
@@ -69,15 +67,9 @@ always @(negedge clk or negedge rst_n)begin
 		end
 		7:begin
 			acc_state <= 9;
-			acc_mux <= 1;
 			acc_buf[2] <= adder_out[`D_LEN-1:0];
 		end
-//		8:begin									//选择输入为三级流水线缓存的数据，先进行前两个数据的加法运算
-//			acc_mux <= 1;
-//			acc_state <= 9;
-//		end
-		9:acc_state <= 10;
-		10:acc_state <= 12;
+		9:acc_state <= 12;					//延时一个时钟
 		12:begin									//再进行后两个数据的加法运算
 			acc_buf[0] <= adder_out[`D_LEN-1:0];
 			acc_buf[1] <= acc_buf[2];
